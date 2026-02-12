36a0a8f1cec8cd5fd926fad688a6010d5bbc4bee
Phunyakrit <phunyakrit@gmail.com>
2026-02-12 21:37:02 +0700

    lab18_1.cpp completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,20 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect r1, Rect r2){
+double overlapX_start = max(r1.x, r2.x);
+double  overlapX_end = min(r1.x + r1.w, r2.x + r2.w);
+double w_overlap = overlapX_end - overlapX_start;
 
+double overlapY_start = min(r1.y, r2.y);
+double overlapY_end = max(r1.y - r1.h, r2.y - r2.h);
+double h_overlap = overlapY_start - overlapY_end;
+
+if(w_overlap <= 0 || h_overlap <= 0){
+	return 0;
+} else{
+	return w_overlap * h_overlap;
+}
 
 
 }
